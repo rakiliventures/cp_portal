53ba85c62f273a5e3962de32b7c42a4d21547a2f
Neville Masheti <mashetin.91@gmail.com>
2026-04-14 20:39:44 +0300

    Runtime error fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  serverExternalPackages: ["bcryptjs", "@prisma/client", "prisma"],
 };
 
 export default nextConfig;
